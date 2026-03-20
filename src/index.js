e165abf40a4c4b2d69ed520d89b447762bb60a0f
shashi <shashiweb64@gmail.com>
2026-03-20 16:54:53 +0530

    create new repo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "./style.css";
+import Myabout,{Mypage, Page3} from './About';
+import Mybootstrap from './Mybootstrap';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Myabout></Myabout>
+    <Mypage></Mypage>
+    <Page3></Page3>
+    <Mybootstrap/>
+  </React.StrictMode> 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
