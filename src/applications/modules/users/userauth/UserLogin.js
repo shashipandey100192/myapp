4b0b25f765f5467632ee4c33e7d4569b77485268
shashi <shashiweb64@gmail.com>
2026-04-08 12:31:32 +0530

    change

--- src/applications/modules/users/userauth/UserLogin.js
+++ src/applications/modules/users/userauth/UserLogin.js
@@ -1,9 +1,53 @@
 import React from 'react'
 import { MdOutlineMailOutline } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { ToastContainer,toast } from 'react-toastify';
 
 function UserLogin() {
+
+  const a = useNavigate();
+
+const [email,updateimail]=useState("abc@gmail.com")
+const [pass,updatepass]=useState("123");
+
+
+const myemailupdate = (e)=>{
+  updateimail(e.target.value)
+}
+
+const mypassword = (p)=>{
+  updatepass(p.target.value)
+}
+
+const mylogin = ()=>{
+    if(email==="")
+    {
+      toast.info("email is required",{autoClose:2000,theme:'dark'});
+    }
+    else if(pass==="")
+    {
+       toast.info("password is required",{autoClose:2000,theme:'dark'});
+    }
+    else
+    {
+      if(email==="admin@gmail.com" && pass==="admin")
+      {
+        toast.success("welcome",{autoClose:2000,theme:'dark'});
+        setTimeout(()=>{
+        a("/dashboard");
+
+        },2000)
+      }
+      else
+      {
+         toast.error("not valid",{autoClose:2000,theme:'dark'});
+      }
+    }
+
+}
+
   return (
     <div className='container'>
       <div className='row justify-content-center'>
@@ -15,20 +59,20 @@ function UserLogin() {
               <div className='col-md-12'>
                 <div class="mb-3">
                   <label class="form-label"> <MdOutlineMailOutline/> Email address</label>
-                  <input type="email" class="form-control" />
+                  <input type="email" class="form-control" value={email} onInput={myemailupdate}/>
                 </div>
               </div>
-
+    <ToastContainer/>
               <div className='col-md-12'>
                 <div class="mb-3">
                   <label class="form-label"> <RiLockPasswordLine/> Password</label>
-                  <input type="password" class="form-control" />
+                  <input type="password" class="form-control" value={pass} onInput={mypassword}/>
                 </div>
               </div>
               <div className='col-md-12 text-center'>
                  <div class="mb-3">
                  
-                  <input type="button" class="btn btn-success" value="login"/>
+                  <input type="button" class="btn btn-success" value="login" onClick={mylogin}/>
                   <Link to="registor" >New User</Link>
                 </div>
               </div>
